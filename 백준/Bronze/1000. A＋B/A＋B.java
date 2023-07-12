13470787a95e95b03fb5044c61711124380e4818
Jina Kong <81066502+JinaKong@users.noreply.github.com>
2023-07-12 11:54:05 +0900

    [Bronze V] Title: A+B, Time: 128 ms, Memory: 14280 KB -BaekjoonHub

--- 백준/Bronze/1000. A＋B/A＋B.java
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -1,12 +1,21 @@
-import java.util.Scanner;
+import java.io.*;
+import java.io.BufferedWriter;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        //입력
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String input = br.readLine();
 
-        int a = sc.nextInt();
-        int b = sc.nextInt();
+        StringTokenizer st = new StringTokenizer(input);
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
 
-        System.out.println(a+b);
+        //출력
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        bw.write(a+b +"");  //String 형으로 변환시켜주어야 한다.
+        bw.flush();
+        bw.close();
     }
 }

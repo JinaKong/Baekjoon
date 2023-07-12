11ffd1b5874ac07b16b2e715d429280a818dc46e
Jina Kong <81066502+JinaKong@users.noreply.github.com>
2023-07-12 20:29:23 +0900

    [Bronze V] Title: 고양이, Time: 120 ms, Memory: 13936 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.java
@@ -0,0 +1,17 @@
+import java.io.*;
+
+public class Main {
+    
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    public static void main(String[] args) throws IOException {
+
+        bw.write("\\    /\\" + "\n");
+        bw.write(" )  ( ')" + "\n");
+        bw.write("(  /  )" + "\n");
+        bw.write(" \\(__)|" + "\n");
+
+        bw.flush();
+        bw.close();
+    }
+}

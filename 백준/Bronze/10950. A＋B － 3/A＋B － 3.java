d49148e1171542489bc9c6ee7ab38274f3de8d6e
Jina Kong <81066502+JinaKong@users.noreply.github.com>
2023-07-13 11:13:08 +0900

    [Bronze V] Title: A+B - 3, Time: 128 ms, Memory: 14272 KB -BaekjoonHub

--- 백준/Bronze/10950. A＋B － 3/A＋B － 3.java
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.java
@@ -11,17 +11,22 @@ public class Main {
         int testNum = 0;  // 반복 개수
         int A,B = 0;        // 정수 A,B
 
+        //StringBuilder 추가!!
+        StringBuilder sb = new StringBuilder();
+
         testNum = Integer.parseInt(br.readLine());
         for(int i=0; i<testNum; i++) {
-            
+
             StringTokenizer st = new StringTokenizer(br.readLine());
             A = Integer.parseInt(st.nextToken());
             B = Integer.parseInt(st.nextToken());
-            
-            bw.write(A+B +"\n");    // 바로 출력
+
+            sb.append(A+B); // StringBuilder에 문자열 연결
+            sb.append("\n");
         }
         br.close();
-        
+
+        bw.write(sb.toString());    // 출력
         bw.flush();
         bw.close();
     }

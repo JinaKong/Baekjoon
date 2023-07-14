9bea70fbdde51a0046c825c0bcf5e694f1ae085f
Jina Kong <81066502+JinaKong@users.noreply.github.com>
2023-07-14 15:30:27 +0900

    [Bronze V] Title: A+B - 7, Time: 132 ms, Memory: 14168 KB -BaekjoonHub

---
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    //static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    public static void main(String[] args) throws IOException {
+
+        int N = 0;  // 테스트케이스의 개수
+        int sum=0;
+
+        StringBuilder sb = new StringBuilder();
+
+        N = Integer.parseInt(br.readLine());
+        for(int i=0; i<N; i++){
+            String input[] = br.readLine().split(" ");
+            sum = Integer.parseInt(input[0]) + Integer.parseInt(input[1]);
+
+            sb.append("Case #").append(i+1).append(": ");
+            sb.append(sum).append("\n");
+        }
+        br.close();
+
+        System.out.println(sb);
+    }
+}

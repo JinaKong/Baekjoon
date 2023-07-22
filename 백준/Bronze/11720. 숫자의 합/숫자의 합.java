e7d08f8b53a63c822d634323e1123b9b5751bcc2
Jina Kong <81066502+JinaKong@users.noreply.github.com>
2023-07-22 18:23:58 +0900

    [Bronze IV] Title: 숫자의 합, Time: 128 ms, Memory: 14156 KB -BaekjoonHub

--- 백준/Bronze/11720. 숫자의 합/숫자의 합.java
+++ 백준/Bronze/11720. 숫자의 합/숫자의 합.java
@@ -8,15 +8,16 @@ public class Main {
 
         // 입력받기
         int N = Integer.parseInt(br.readLine());    // 입력 숫자의 개수
-        char[] chars = br.readLine().toCharArray();
+        char[] ch = br.readLine().toCharArray();
 
         // 숫자 N개의 합 계산
         int sum = 0;
         for(int i=0; i<N; i++){
-            sum += (int)chars[i] - 48 ;
+            //sum += (int)ch[i] - 48 ;
+            sum += Character.getNumericValue(ch[i]);    // char형 숫재를 int형 숫자로 변환
         }
 
-        
+
         // 합 출력
         System.out.println( sum );
     }

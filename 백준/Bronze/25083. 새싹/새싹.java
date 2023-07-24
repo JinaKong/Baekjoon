ee0dd85a8b8549e972d97585682e9867711a43aa
Jina Kong <81066502+JinaKong@users.noreply.github.com>
2023-07-24 23:23:09 +0900

    [Bronze V] Title: 새싹, Time: 108 ms, Memory: 13948 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.java
@@ -0,0 +1,20 @@
+import java.io.*;
+
+public class Main {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void main(String[] args) throws IOException {
+        
+        StringBuilder sb= new StringBuilder();
+
+        sb.append("         ,r'\"7\n");
+        sb.append("r`-_   ,'  ,/\n");
+        sb.append(" \\. \". L_r'\n");
+        sb.append("   `~\\/\n");
+        sb.append("      |\n");
+        sb.append("      |");
+
+        System.out.println(sb);
+    }
+}

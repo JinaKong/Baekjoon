b7e1fa4003d092c6b0f52a3e6a1c97fd8d5da66a
Jina Kong <81066502+JinaKong@users.noreply.github.com>
2023-07-25 22:47:00 +0900

    Update 팰린드롬인지 확인하기.java

--- 백준/Bronze/10988. 팰린드롬인지 확인하기/팰린드롬인지 확인하기.java
+++ 백준/Bronze/10988. 팰린드롬인지 확인하기/팰린드롬인지 확인하기.java
@@ -15,3 +15,32 @@ public class Main {
         br.close();
     }
 }
+
+/* 또 다른 풀이법
+
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        // 단어 입력받기
+        String input = br.readLine();
+        int L = input.length();   // 단어의 길이
+        
+
+        // 단어의 앞뒤 같은지 확인
+        int flag = 1;   // flag를 1로 시작
+        
+        for(int i=0; i < L/2; i++){
+            if( input.charAt(i) != input.charAt(L-i-1) ){
+                flag = 0;   // 다르면 flag를 0으로
+            }
+        }
+
+        // 출력
+        System.out.println(flag);
+    }
+}
+*/

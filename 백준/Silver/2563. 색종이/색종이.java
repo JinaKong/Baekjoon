be81182c064d42d5eff7cab1e711cdf4d5fcd403
Jina Kong <81066502+JinaKong@users.noreply.github.com>
2023-08-11 14:38:08 +0900

    [Silver V] Title: 색종이, Time: 120 ms, Memory: 14220 KB -BaekjoonHub

--- 백준/Silver/2563. 색종이/색종이.java
+++ 백준/Silver/2563. 색종이/색종이.java
@@ -6,64 +6,35 @@ public class Main {
 
         BufferedReader br = new BufferedReader( new InputStreamReader(System.in));
 
-        /*
-        입력
-         */
+        boolean Area[][] = new boolean [101][101];  // 도화지(기본 false)
         int N = Integer.parseInt(br.readLine());    // 색종이의 수
+        int x, y;        // 색종이의 위치
+        int area = 0;    // 색종이가 있는 면적
+
         String input;
-        int x[] = new int[N];
-        int y[] = new int[N];
-
-        /*
-        입력
-         */
         for(int i=0; i<N; i++) {
+
+            // 색종이의 위치 입력받기
             input = br.readLine();
-            x[i] = Integer.parseInt(input.split(" ")[0]);
-            y[i] = Integer.parseInt(input.split(" ")[1]);
-        }
+            x = Integer.parseInt(input.split(" ")[0]);
+            y = Integer.parseInt(input.split(" ")[1]);
 
-        /*
-        색종이가 있는 영역 계산
-         */
-        boolean Area[][] = new boolean [101][101];
+            // 해당 위치+10까지 true로 변경
+            for(int j = x; j < x+10; j++) {
+                for(int k= y; k < y+10; k++) {
 
-        for(int i = 0; i < N; i++) {
+                    if(Area[j][k] == false){
+                        Area[j][k] = true;
+                        area ++;
+                    }
 
-            for(int j = x[i]; j < x[i]+10; j++) {
-                for(int k= y[i]; k<y[i]+10; k++) {
-                    Area[j][k] = true;
-                    //System.out.println(j+", " +k  +" ");
                 }
             }
         }
 
-        /*
-        출력
-         */
-
-        /*
-        for(int i=0; i<100; i++){
-            for (int j=0; j<100; j++) {
-                if (Area[i][j] == true) {
-                    //System.out.println(i+", " +j  +" ");
-                    System.out.print("■");
-                } else
-                    System.out.print("□");
-            }
-            System.out.println();
-        }
-
-         */
-
-        int count = 0;
-        for(int i=0; i<=100; i++){
-            for(int j=0; j<=100; j++){
-                if(Area[i][j] == true)
-                    count++;
-            }
-        }
-        System.out.println(count);
+        
+        // 출력
+        System.out.println(area);
 
     }
 }

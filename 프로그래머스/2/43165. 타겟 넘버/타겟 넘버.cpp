24bbb2049fba4a869486ced218d34a4bc427fcce
Jina Kong <81066502+JinaKong@users.noreply.github.com>
2023-11-05 13:34:56 +0900

    [level 2] Title: 타겟 넘버, Time: 0.06 ms, Memory: 4.11 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.cpp
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.cpp
@@ -1,35 +1,34 @@
 #include <string>
 #include <vector>
-#include <iostream>
+#include <queue> // BFS 
 
 using namespace std;
 
-//bool is_visited[20]; 
-int answer = 0;
-
-// dfs 함수
-void dfs(vector<int> numbers, int target, int sum, int step){  
-    //base condition
-    if(step == numbers.size()){
-        if(sum == target) answer ++;
-        return;
-    }
-    
-    //for(int i=step; i<numbers.size(); i++){
-            //if(!is_visited[i]){
-                //is_visited[i] = true;
-                
-                // dfs 두번 호출
-                dfs(numbers, target, sum + numbers[step], step+1); //sum에서 numbers[i] 빼서 호출
-                dfs(numbers, target, sum - numbers[step], step+1); //sum에서 numbers[i] 더해서 호출
-                //is_visited[i] = false;
-           // }
-    //}
-    //return;
-}
-
 int solution(vector<int> numbers, int target) {
-    
-    dfs(numbers, target, 0, 0);
-    return answer;
+    int answer = 0; // 결과를 저장할 변수
+    int cnt = 0; // 현재 처리 중인 숫자의 인덱스
+
+    queue<int> q; // BFS를 위한 큐
+    q.push(0); // 초기값을 큐에 넣음
+
+    while (cnt < numbers.size()) { // 숫자 목록을 모두 처리할 때까지 반복
+        int size = q.size(); // 현재 큐의 크기를 저장 (현재 레벨의 노드 수)
+
+        for (int i = 0; i < size; i++) { // 현재 레벨의 노드들에 대해 반복
+            int x = q.front(); // 큐에서 값을 꺼내옴
+            q.pop(); // 꺼낸 값은 큐에서 제거
+
+            q.push(x + numbers[cnt]); // 현재 숫자를 더해서 큐에 추가
+            q.push(x - numbers[cnt]); // 현재 숫자를 뺴서 큐에 추가
+        }
+        
+        cnt++; // 다음 숫자로 이동
+    }
+
+    while (!q.empty()) { // 큐가 비어있을 때까지 반복
+        if (q.front() == target) answer++; // 큐의 첫 번째 값이 목표값과 일치하면 결과를 증가
+        q.pop(); // 큐에서 값을 제거
+    }
+
+    return answer; // 최종 결과 반환
 }

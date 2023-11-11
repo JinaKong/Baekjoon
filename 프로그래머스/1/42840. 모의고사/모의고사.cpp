a8207e69339d1b1259367f6bf98b1d7f81e87c3f
Jina Kong <81066502+JinaKong@users.noreply.github.com>
2023-11-12 00:35:00 +0900

    [level 1] Title: 모의고사, Time: 0.27 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.cpp
+++ 프로그래머스/1/42840. 모의고사/모의고사.cpp
@@ -1,36 +1,40 @@
+#include <string>
 #include <vector>
 #include <iostream>
 
 using namespace std;
 
+vector<vector<int>> rule = {{1, 2, 3, 4, 5}, {2, 1, 2, 3, 2, 4, 2, 5}, {3, 3, 1, 1, 2, 2, 4, 4, 5, 5}};
+
 vector<int> solution(vector<int> answers) {
     vector<int> answer;
     
-    // 수포자들이 정답을 찍는 패턴
-    vector<vector<int>> pattern = {{1,2,3,4,5}, {2, 1, 2, 3, 2, 4, 2, 5}, {3, 3, 1, 1, 2, 2, 4, 4, 5, 5}};
-    
-    // 각 수포자의 정답수를 저장
-    vector<int> cnt(3, 0);  // 3명 모두 0으로 초기화
-    for(int i = 0; i < answers.size(); i++) {
-    
-        for (int j = 0; j < 3; j++) {
-            int k = i % pattern[j].size();  // 패턴이 반복되므로 mod 활용
-            if (answers[i] == pattern[j][k]) {
-                cnt[j]++;   
-            }
+    // 페어로 선언
+    vector<pair<int, int>> score = {{0,1}, {0,2}, {0,3}};
+
+    // 맞춘 개수 페어에 저장
+    for(int i=0; i<3; i++){                                
+        for(int j=0; j<answers.size(); j++){
+            int num = j % rule[i].size();
+            //cout << num << ' ';
+            //cout << rule[i][num] << ' ';
+            if(answers[j] == rule[i][num])
+                score[i].first++;
         }
+        cout << score[i].first <<' ';
+        //cout << endl;
     }
-
-    // 가장 cnt가 높은 수포자 찾기
-    int maxCount = max(cnt[0], cnt[1]);
-    maxCount = max(maxCount, cnt[2]);
-
-    // answer 벡터에 저장해서 return (여러명이면 오름차순)
-    for (int i = 0; i < 3; i++) {
-        if (cnt[i] == maxCount) {
-            answer.push_back(i + 1); // 사람 번호는 1부터 시작
-        }
+        
+    // 최댓값 찾기
+    int max_score = max(score[0].first, score[1].first);
+    max_score = max(max_score, score[2].first);
+    //cout << max_score;
+        
+    // 정답 출력
+    for(int i=0; i<3; i++){
+        if(score[i].first == max_score)
+            answer.push_back(i+1);
     }
-
+    
     return answer;
 }
